a8ac7a18f216fc5ad7647249a5c2974b5751b44b
jarikvasil <jarikvasil@mail.ru>
2019-12-22 23:17:42 +0300

    ##

--- emogic-test/src/components/LoginForm.js
+++ emogic-test/src/components/LoginForm.js
@@ -1,6 +1,6 @@
 ﻿import React from 'react';
 
-const URL = "http://localhost:3000/";
+const URL = "http://localhost:8080/";
 
 class LoginForm extends React.Component{
 	
@@ -15,7 +15,7 @@ class LoginForm extends React.Component{
 	handleSubmit = async (event) => {
 		event.preventDefault();
 		const body = {login: this.state.login, password: this.state.password};
-		const response = await fetch(URL, {method: "POST", headers: {"Content-Type": "application/json;charset=utf-8"}, body: JSON.stringify(body)});
+		const response = await fetch(URL, {method: "POST", headers: {"Content-Type": "text/plain;charset=utf-8"}, body: JSON.stringify(body)});
 		console.log(response);
 	}
 	
@@ -31,22 +31,24 @@ class LoginForm extends React.Component{
 	
 	render(){
 		return (
-				<form onSubmit={this.handleSubmit}>
-					<div>
-						<div className="form-group col-xs-4">
-							<label for="login-input">Логин:</label>
-							<input type="text" id="login-input" className="form-control" value={this.state.login} onChange={this.handleLoginChange}/>
+				<div className="container">
+					<form onSubmit={this.handleSubmit} role="form">
+						<div>
+							<div className="form-group">
+								<label htmlFor="login-input">Логин:</label>
+								<input type="text" id="login-input" className="form-control" value={this.state.login} onChange={this.handleLoginChange}/>
+							</div>
 						</div>
-					</div>
-					<div className="form-group col-xs-4">
-						<label for="password-input">Пароль:</label>
-						<input type="password" id="password-input" className="form-control" value={this.state.password} onChange={this.handlePasswordChange}/>
-					</div>
-					<div className="form-group col-xs-4">
-						<label for="submit-button"></label>
-						<input type="submit" id="submit-button" className="btn btn-primary" value="Войти"/>
-					</div>	
-				</form>
+						<div className="form-group">
+							<label htmlFor="password-input">Пароль:</label>
+							<input type="password" id="password-input" className="form-control" value={this.state.password} onChange={this.handlePasswordChange}/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="submit-button"></label>
+							<input type="submit" id="submit-button" className="btn btn-primary" value="Войти"/>
+						</div>	
+					</form>
+				</div>
 		)
 	}
 }
